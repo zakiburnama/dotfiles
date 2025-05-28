8df127acd8b6d246813cf39f23d5b7435ac76f9f
zakiburnama <zaki.burnama1017@gmail.com>
2025-05-28 11:20:25 +0700

    update color scheme to have more theme

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  -- Tokyonight theme
+  { "folke/tokyonight.nvim" },
+
+  -- Gruvbox theme
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "vague2k/vague.nvim" },
+
+  --  -- Configure LazyVim to load gruvbox
+  --  {
+  --    "LazyVim/LazyVim",
+  --    opts = {
+  --      colorscheme = "gruvbox",
+  --    },
+  --  },
+}
